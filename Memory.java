643a874f46f17e9308773071ee7778def6d55851
Mayur2k2 <kumarmayur153@gmail.com>
2025-07-31 14:26:11 +0530

    File Uploaded

--- Memory.java
+++ Memory.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Memory {
 
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> cards = new ArrayList<>();
